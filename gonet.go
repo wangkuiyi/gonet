5a445f3d41d37fdbe647be458c22714a2f07fd53
Go Dist 1 <go.bigdata.learning@gmail.com>
2014-07-07 17:55:28 -0700

    Add function comments

--- gonet.go
+++ gonet.go
@@ -17,13 +17,24 @@ type Chan struct {
 }
 
 var (
-	namedChans map[string]*Chan
+	namedChans map[string]*Chan // named channels will be registered here.
 )
 
 func init() {
 	namedChans = make(map[string]*Chan)
 }
 
+// MakeChan creates a Gonet channel, which collects typed data from
+// the network, and returns the read-end Go channel.  MakeChan is
+// supposed to be called by the data reader process.  A made channel
+// is named by the network address that the reader is listening on.
+// From technical perspective, each channel is associated with a TCP
+// listener and a connection acceptance goroutine.  Thus, writers can
+// make network connections through this listener and feed data
+// through these connections.  Note that the maximum number of
+// concurrent connections is limited by the global variable
+// kMaxOpeningChannels.  This limitation protects the reader/server
+// process from being too busy in channel I/O.
 func MakeChan(addr string, value interface{}) (
 	chan interface{}, error) {
 
@@ -89,6 +100,11 @@ func transcode(conn net.Conn, value interface{}, valueChan chan interface{}) {
 	}
 }
 
+// CloseChan closes a named channel if it is created, or does nothing
+// otherwise.  The reader/server process which called MakeChan to
+// create the channel is expected to call CloseChan when it does no
+// longer want to serve this channel.  This frees the network address
+// (port) that the process is listening on.
 func CloseChan(addr string) {
 	if ch, ok := namedChans[addr]; ok {
 		ch.listener.Close()
@@ -97,6 +113,10 @@ func CloseChan(addr string) {
 	}
 }
 
+// OpenChan opens the write-end channle of a Gonet channel.  It is
+// expected to be called by a process which wants to feed data into
+// the Gonet channel.  After the feeding, the caller process simply
+// close this write-end Go channel to free all related resource.
 func OpenChan(addr string) (chan interface{}, error) {
 	conn, e := net.Dial("tcp", addr)
 	if e != nil {
